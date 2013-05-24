f84e034188e87805d6dc2b580e173abfac752934
Rahul Sharma <rsharma@apache.org>
2013-05-24 09:14:58 +0530

    Addind tycho-surefire plugin for unit testing : - can run from eclipse as Junit plugin testing - Can run from maven as 'mvn integration-test'

---
+++ org.apache.hdt.core.test/src/org/apache/hdt/core/cluster/HadoopClusteTest.java
@@ -0,0 +1,14 @@
+package org.apache.hdt.core.cluster;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HadoopClusteTest {
+
+	@Test
+	public void shouldFail() {
+		fail("PDE testing works !! just implement this");
+	}
+
+}
